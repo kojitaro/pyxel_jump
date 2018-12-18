35896ccc8ee2212678d25cf78cbe88998e89f45e
Kouji Ohura <ohura@anthill.jp>
2018-12-19 05:54:21 +0900

    ゲームのとりあえずの作りを実装

--- game.py
+++ game.py
@@ -2,13 +2,22 @@ import pyxel
 import Box2D
 from Box2D import b2
 
+from enum import Enum
+
 PPM = 16.0  # pixels per meter
 TARGET_FPS = 30
 TIME_STEP = 1.0 / TARGET_FPS
 SCREEN_WIDTH, SCREEN_HEIGHT = 224, 126
 SCREEN_Y_ZERO_OFFSET = 120
 STAGE_WIDTH_M = 70  # m
+STAGE_ITEMEND_X_M = 67  # ここまでしかものが置かない
 
+SCREEN_WIDTH_M = int(SCREEN_WIDTH/PPM)
+SCREEN_HIGHT_M = int(SCREEN_HEIGHT/PPM)
+
+DEBUG_DRAW=True
+
+STAGE_NUM = 10
 
 
 # プレイヤーの移動の起点
@@ -18,28 +27,95 @@ PLAYER_SCROLL_X_M = 3
 DRAW_OFFSET_X = 0
 DRAW_OFFSET_X_M = 0
 
+SPRITE_SETS = [
+    [0,0,16,16], # プレイヤー
+    [0,16,16,16],
+    [0, 48, 16, 16], # コイン
+    [0, 64, 16, 16], # わな
+    [0, 32, 16, 16], # 旗
+]
+class SpriteSet(Enum):
+    PLAYER = 0
+    COIN = 2
+    TRAP = 3
+    FLAG = 4
+
+    @staticmethod
+    def map_rect(n):
+        if isinstance(n, SpriteSet):
+            n = n.value
+        if n<0 or n >= len(SPRITE_SETS):
+            return None
+        return SPRITE_SETS[n]
+
+def draw_sprite_world(pos, sprite_n):
+    pos = world_to_draw_pos(pos)
+
+    r = SpriteSet.map_rect(sprite_n)
+    if r is None:
+        return
+    w = r[2]
+    h = r[3]
+
+
+    pyxel.blt(pos[0]-w/2 + DRAW_OFFSET_X,
+        pos[1]-h/2,
+        0,
+        r[0],r[1],r[2],r[3],
+        11)
+
+def draw_body_sprite(body, sprite_n):
+    pos = body_pos(body)
+    draw_sprite_world(pos, sprite_n)
+
+def draw_sprite(pos, sprite_n):
+    r = SpriteSet.map_rect(sprite_n)
+    if r is None:
+        return
+    pyxel.blt(pos[0] + DRAW_OFFSET_X,
+        pos[1],
+        0,
+        r[0],r[1],r[2],r[3],
+        11)
+
+# 描画の全体のオフセット値を設定
 def set_draw_offset_x_m(offset_m):
     global DRAW_OFFSET_X_M
     global DRAW_OFFSET_X
 
     DRAW_OFFSET_X_M = offset_m
-    DRAW_OFFSET_X = offset_m * PPM
+    DRAW_OFFSET_X = int(offset_m * PPM)
 
+# ボディの位置を取得　ワールド座標
 def body_pos(body):
     return body.transform * (0,0)
 
+def set_body_pos(body, pos):
+    body.transform = (pos, 0)
+
+
+# ボディの位置を取得　描画座標
 def body_draw_pos(body):
     return world_to_draw_pos(body.transform * (0,0))
 
+# 位置をワールド座標から描画座標に変換
 def world_to_draw_pos(pos):
     return int(pos[0]* PPM), int(SCREEN_Y_ZERO_OFFSET - pos[1] * PPM)
 
+# ポリゴンを描画
+# 引数は描画座標の位置
 def draw_polygon(vertices, color):
-    for i in range(len(vertices)-1):
+    n = len(vertices)
+    for i in range(n-1):
         pyxel.line(vertices[i][0] + DRAW_OFFSET_X, vertices[i][1],
             vertices[i+1][0] + DRAW_OFFSET_X, vertices[i+1][1],
             color)
 
+    pyxel.line(vertices[n-1][0] + DRAW_OFFSET_X, vertices[n-1][1],
+        vertices[0][0] + DRAW_OFFSET_X, vertices[0][1],
+        color)
+
+# ボディを描画
 def draw_body(body, color):
     for fixture in body.fixtures:
         shape = fixture.shape
--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python
 import os
+import random
+import math
 
 import pyxel
 from Box2D import b2
 
 import game as g
+
+# class GameMange:
+#     def __init__(self):
+#         self.coins = 0
+#         self.stages = [False] * g.STAGE_NUM
+    
+#     def 
 
 
 class Player(g.GContainer):
@@ -12,71 +21,148 @@ class Player(g.GContainer):
         super().__init__()
         self.walk_counter = 0
 
-        self.body = world.CreateDynamicBody(position=(2, 4), angle=0)
-        self.body.CreateCircleFixture(radius=0.5, density=1, friction=0.3)
+        self.first_pos = (2,4)
+        self.hit = False
+
+        self.body = world.CreateDynamicBody(position=self.first_pos, angle=0)
+        self.body.CreateCircleFixture(radius=0.45, density=1, friction=0.3)
 
     def update(self):
         super().update()
 
-        if self.body.linearVelocity[1] == 0:
-            if pyxel.btn(pyxel.KEY_LEFT):
-                self.body.angularVelocity = 20
-            elif pyxel.btn(pyxel.KEY_RIGHT):
-                self.body.angularVelocity = -20
-            else:
-                self.body.angularVelocity = 0
+        x = 0
+        if pyxel.btn(pyxel.KEY_LEFT):
+            x = -5
+            self.walk_counter += 1
+        elif pyxel.btn(pyxel.KEY_RIGHT):
+            x = 5
+            self.walk_counter += 1
 
+        self.body.linearVelocity = (x, self.body.linearVelocity[1])
+        if self.body.linearVelocity[1] == 0:
             if pyxel.btn(pyxel.KEY_X) :
                 self.body.linearVelocity = (self.body.linearVelocity[0], 7)
+        else:
+            self.walk_counter = 0
+
+        pos = g.body_pos(self.body)
+        if pos[1] < -10:
+            self.hit = True
+        if pos[0] >= g.STAGE_WIDTH_M:
+            self.hit = True
+
+        if self.hit:
+            #  落ちた
+            self.body.linearVelocity = (0,0)
+            g.set_body_pos(self.body, self.first_pos)
+            self.hit = False
+
+        
+
 
     def draw(self):
         super().draw()
 
-        g.draw_body(self.body, 8)
+        if g.DEBUG_DRAW:
+            g.draw_body(self.body, 8)
+        g.draw_body_sprite(self.body, g.SpriteSet.PLAYER.value+int(self.walk_counter/4)%2)
 
-        plps = [[0,0,16,16],[0,16,16,16]]
-        plp = plps[int(self.walk_counter/4)%2]
-        #self.walk_counter
+class GroundGenerator:
+    def __init__(self, a):
+        random.seed(a)
+        self.w = g.STAGE_ITEMEND_X_M-g.PLAYER_SCROLL_X_M-2
+        self.h = g.SCREEN_HIGHT_M
+        self.items = [False] * (self.w*self.h)
 
-        pos = g.body_draw_pos(self.body)
-        pyxel.blt(g.DRAW_OFFSET_X + pos[0]-16/2,
-            pos[1]-16/2,
-            0,
-            plp[0],plp[1],plp[2],plp[3], 11)
+    def next(self):
+        while True:
+            n = random.randint(0, self.w*self.h-1)
+            if self.items[n]:
+                next
+            self.items[n] = True
+
+            return (g.PLAYER_SCROLL_X_M+2 + int(n % self.w),
+                int(n / self.w)+0.5)
+            
+
 
 class Ground(g.GContainer):
-    def __init__(self, world):
+    def __init__(self, world, generator):
         super().__init__()
 
-        self.ground_body = world.CreateStaticBody(
-            position=(g.STAGE_WIDTH_M/2, -1),
-            shapes=b2.polygonShape(box=(g.STAGE_WIDTH_M/2, 2)),
-        )
-    
+        self.bodys = []
+        
+        for n in range(0, g.STAGE_WIDTH_M+(g.SCREEN_WIDTH_M-g.PLAYER_SCROLL_X_M)):
+            body = world.CreateStaticBody(
+                position=(n-0.5, -0.1),
+                shapes=b2.polygonShape(box=(1, 0.05)),
+            )
+            self.bodys.append( body )
+
+
+        # 板
+        for n in range(50):
+            pos = generator.next()
+            body = world.CreateStaticBody(
+                position=pos,
+                shapes=b2.polygonShape(box=(0.5, 0.5))
+                )
+            self.bodys.append(body)
+
+        self.coins = []
+        for n in range(15):
+            self.coins.append( generator.next() )
+
+        self.traps = []
+        for n in range(15):
+            self.traps.append( generator.next() )
+
+    def update(self, player):
+        pb = g.body_pos(player.body)
+
+        for s in self.coins[:]:
+            d = math.sqrt((pb[0]-s[0])*(pb[0]-s[0])+(pb[1]-s[1])*(pb[1]-s[1]))
+            if d <= 0.8:
+                self.coins.remove(s)
+
+        for s in self.traps[:]:
+            d = math.sqrt((pb[0]-s[0])*(pb[0]-s[0])+(pb[1]-s[1])*(pb[1]-s[1]))
+            if d <= 0.8:
+                player.hit = True
+
     def draw(self):
         super().draw()
-        g.draw_body(self.ground_body, 3)
+
+        # 地面を描画
+        for body in self.bodys:
+            g.draw_body(body, 3)
 
         # 旗描画
-        pos = g.world_to_draw_pos((g.STAGE_WIDTH_M, 0))
-        pyxel.blt(g.DRAW_OFFSET_X + pos[0]-16,
-            pos[1]-16,
-            0,
-            0, 32, 16, 16, 11)
+        g.draw_sprite_world(((g.STAGE_WIDTH_M, 0.5)), g.SpriteSet.FLAG)
 
+        # コインを描画
+        for coin in self.coins:
+            g.draw_sprite_world(coin, g.SpriteSet.COIN)
+
+        # わなを描画
+        for trap in self.traps:
+            g.draw_sprite_world(trap, g.SpriteSet.TRAP)
+            
 
 
 
 
 class SceneMain(g.Scene):
     def __init__(self):
+        generator = GroundGenerator(0)
 
         self.world = b2.world(gravity=(0, -10), doSleep=True)
         self.player = Player(self.world)
-        self.ground = Ground(self.world)
+        self.ground = Ground(self.world, generator)
 
     def update(self):
         self.world.Step(g.TIME_STEP, 10, 10)
+        self.ground.update(self.player)
         self.player.update()
 
     def draw(self):
